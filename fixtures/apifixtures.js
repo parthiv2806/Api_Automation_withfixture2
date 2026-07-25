4c9b2c9837d384888657cd7db778908119bd5ca7
parthiv2806 <parthivbhavsar2001@gmail.com>
2026-07-25 13:51:36 +0530

    Add the booking id fixture

--- fixtures/apifixtures.js
+++ fixtures/apifixtures.js
@@ -1,9 +1,17 @@
 import { test as base } from "@playwright/test";
 import { Generatetoken } from "../utils/tokenmanager";
-
+import { Create_booking } from "../services/createservices";
+import { CreateBooking } from "../payloads/createBookingPaylod";
 export const test = base.extend({
   token: async ({ request }, use) => {
     const token = await Generatetoken(request);
     await use(token);
+  },
+  bookingId: async ({ request }, use) => {
+    const response = await Create_booking(request, CreateBooking);
+
+    const body = await response.json();
+
+    await use(body.bookingid);
   },
 });
